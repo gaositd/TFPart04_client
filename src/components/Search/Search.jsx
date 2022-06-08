0ebf2b6293f097b2211d49abe924c1f61e845fb4
Lucas <l.ucase1840@gmail.com>
2022-06-08 16:55:15 -0300

    Refactoring

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import style from './Search.module.css'
 import { byName } from '../../redux/actions.js'
 
 const Search = ({ allProducts }) => {
