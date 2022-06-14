17ef8466e652b5d2adf4f98e5675174bf6c2e02b
Santiago Andrés Testa <testasanty@gmail.com>
2022-06-14 16:24:34 -0300

    Signup and Login for gmail only with Google Signup

--- src/components/Firebase/FirebaseSignIn.jsx
+++ src/components/Firebase/FirebaseSignIn.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { auth, provider } from '../../firebase'
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signUp } from '../../redux/actions';
 
 const FirebaseSignIn = () => {
+  const dispatch = useDispatch();
 
   const navigate = useNavigate();
 
@@ -11,21 +14,27 @@ const FirebaseSignIn = () => {
     try {
       return await signInWithPopup(auth, provider)
     } catch (error) {
-      console.log('Signin with google error: ',error)
+      console.log('Signin with google error: ', error)
     }
   }
 
   async function handleGoogleSignin() {
     const googleResponse = await signInWithGoogle()
-    console.log(googleResponse.user.email)
-    if(googleResponse.user.email) {
-      localStorage.setItem("user", googleResponse.user.email)
-      localStorage.setItem("usertype", 'User')
-      navigate('/home')
+    console.log(googleResponse)
+    if (googleResponse.user.email) {
+      const googleUser = {
+        email: googleResponse.user.email,
+        password: googleResponse.user.uid.slice(0,10),
+        nickName: googleResponse.user.email.split('@')[0]
+      }
+      await dispatch(signUp(googleUser))
+      if (localStorage.user) {
+        navigate('/home')
+      }
     } else alert('No response from google')
     // await dispatch(login(googleResponse.user.email))
     // if (localStorage.user) {
-      // navigate('/home')
+    // navigate('/home')
     // }
   }
 
